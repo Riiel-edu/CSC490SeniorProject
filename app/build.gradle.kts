94e780b03522164ce2171e7a4b0622c698b2a62e
AditheD <adithedas20@gmail.com>
2026-09-18 20:10:30 -0400

    build: update authentication dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
-    id("com.google.gms.google-services") version "4.5.0"
+    //id("com.google.gms.google-services") version "4.5.0"
 
 
 }
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
